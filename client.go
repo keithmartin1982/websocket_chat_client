dbb1834c47f2dfea5f657ec1160470bf4dc62522
keithmartin1982 <florida.dev.0@proton.me>
2025-10-20 15:02:36 -0400

    init

---
+++ client.go
@@ -0,0 +1,113 @@
+package chat
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	Conn        *websocket.Conn
+	Addr        string
+	Port        string
+	Proto       string
+	wsPath      string
+	RoomID      string
+	RoomPass    string
+	MessageKey  string
+	Username    string
+	MessageChan chan struct {
+		Type int
+		Data []byte
+	}
+}
+
+type ConnectMessage struct {
+	ID       string `json:"id"`
+	Password string `json:"p"`
+}
+
+type Msg struct {
+	Username string `json:"un"`
+	Message  string `json:"msg"`
+}
+
+func (c *Client) Connect() error {
+	var err error
+	c.wsPath = "message_ws"
+	c.Conn, _, err = websocket.DefaultDialer.Dial(fmt.Sprintf("%s://%s:%s/%s", c.Proto, c.Addr, c.Port, c.wsPath), nil)
+	if err != nil {
+		return fmt.Errorf("dial: %v", err)
+	}
+	c.listener()
+	loginJson, err := json.Marshal(ConnectMessage{
+		ID:       c.RoomID,
+		Password: c.RoomPass,
+	})
+	if err != nil {
+		return fmt.Errorf("error: json marshal: %v", err)
+	}
+	err = c.Conn.WriteMessage(websocket.TextMessage, loginJson)
+	if err != nil {
+		return fmt.Errorf("write:", err)
+	}
+	if err := c.SendMsg(" <- has entered the room!"); err != nil {
+		log.Printf("error: sendMsg: %v", err)
+	}
+	return err
+}
+
+func (c *Client) listener() {
+	go func() {
+		for {
+			mt, message, err := c.Conn.ReadMessage()
+			if err != nil {
+				return
+			}
+			var jsonString []byte
+			switch mt {
+			case websocket.TextMessage:
+				if jsonString, err = decrypt([]byte(c.MessageKey), message); err != nil {
+					log.Println("decrypt:", err)
+				}
+			default:
+				jsonString = message
+			}
+			// TODO : decrypt Data before sending in channel
+			c.MessageChan <- struct {
+				Type int
+				Data []byte
+			}{Type: mt, Data: jsonString}
+		}
+	}()
+}
+
+func (c *Client) SendMsg(msg string) error {
+	m := Msg{
+		Username: c.Username,
+		Message:  msg,
+	}
+	jm, err := json.Marshal(m)
+	if err != nil {
+		return fmt.Errorf("error: json marshal: %v", err)
+	}
+	encBytes, err := encrypt([]byte(c.MessageKey), jm)
+	if err != nil {
+		return fmt.Errorf("encrypt: %v", err)
+	}
+	return c.Conn.WriteMessage(websocket.TextMessage, encBytes)
+	
+}
+
+func (c *Client) disconnect() {
+	err := c.Conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+	if err != nil {
+		log.Println("write close:", err)
+		return
+	}
+	if err := c.Conn.Close(); err != nil {
+		log.Printf("error: websocket Conn close: %v", err)
+	}
+}
---
+++ encryption.go
@@ -0,0 +1,69 @@
+package chat
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"io"
+	
+	"golang.org/x/crypto/pbkdf2"
+)
+
+func encrypt(password, plaintext []byte) ([]byte, error) {
+	salt := make([]byte, 16)
+	if _, err := io.ReadFull(rand.Reader, salt); err != nil {
+		return nil, fmt.Errorf("error generating salt: %v", err)
+	}
+	key := pbkdf2.Key(password, salt, 1000, 32, sha256.New)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("aes.NewCipher: %w", err)
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("NewGCM: %s", err)
+	}
+	iv := make([]byte, 12)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, fmt.Errorf("failed to generate iv: %s", err)
+	}
+	ciphertext := gcm.Seal(nil, iv, plaintext, nil)
+	hexSalt := make([]byte, hex.EncodedLen(len(salt)))
+	hex.Encode(hexSalt, salt)
+	hexIv := make([]byte, hex.EncodedLen(len(iv)))
+	hex.Encode(hexIv, iv)
+	hexCiphertext := make([]byte, hex.EncodedLen(len(ciphertext)))
+	hex.Encode(hexCiphertext, ciphertext)
+	return append(append(append(append(hexSalt, []byte("-")...), hexIv...), []byte("-")...), hexCiphertext...), nil // lol
+}
+
+func decrypt(password, cipherText []byte) ([]byte, error) {
+	if !bytes.ContainsAny(cipherText, "-") {
+		return nil, fmt.Errorf("invalid data")
+	}
+	data := bytes.Split(cipherText, []byte("-"))
+	salt := make([]byte, hex.DecodedLen(len(data[0])))
+	hex.Decode(salt, data[0])
+	iv := make([]byte, hex.DecodedLen(len(data[1])))
+	hex.Decode(iv, data[1])
+	ciphertext := make([]byte, hex.DecodedLen(len(data[2])))
+	hex.Decode(ciphertext, data[2])
+	key := pbkdf2.Key(password, salt, 1000, 32, sha256.New)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("decrypt failed: %v", err)
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, fmt.Errorf("decrypt failed: %v", err)
+	}
+	plaintext, err := gcm.Open(nil, iv, ciphertext, nil)
+	if err != nil {
+		return nil, fmt.Errorf("decrypt failed: %v", err)
+	}
+	return plaintext, nil
+}
